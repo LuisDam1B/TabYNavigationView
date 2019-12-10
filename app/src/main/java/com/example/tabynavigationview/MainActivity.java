deee1bd972c74f2a0f5bc8b53fedd98fdfdeb395
Luis Naŕvaez <luis.narvaez@iesdoctorbalmis.com>
2019-12-10 23:35:52 +0100

    Commmit Añadir menu(Fallo), Y Tabs

--- app/src/main/java/com/example/tabynavigationview/MainActivity.java
+++ app/src/main/java/com/example/tabynavigationview/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.tabynavigationview;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager.widget.ViewPager;
 
+
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout mDrawerLaout;
+    TabLayout tabLayout;
+    ViewPager mViewPager;
+    TextView texto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        mDrawerLaout = findViewById(R.id.drawler_layout);
+
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_launcher_background);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        tabLayout = findViewById(R.id.tabLayout);
+
+        tabLayout.addTab(tabLayout.newTab().setText("TELÉFONOS"));
+        tabLayout.addTab(tabLayout.newTab().setText("TABLETS"));
+        tabLayout.addTab(tabLayout.newTab().setText("PORTÁTILES"));
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id){
+            case android.R.id.home:
+                mDrawerLaout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
