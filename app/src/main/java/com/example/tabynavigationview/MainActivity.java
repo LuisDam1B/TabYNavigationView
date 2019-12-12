4567517762903f88569debb51733da241a7b7b34
LuisDam1B <luis.narvaez@iesdoctorbalmis.com>
2019-12-12 13:25:57 +0100

    Commit TabYNavigationView add tablayout viewPager

--- app/src/main/java/com/example/tabynavigationview/MainActivity.java
+++ app/src/main/java/com/example/tabynavigationview/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     DrawerLayout mDrawerLaout;
     TabLayout tabLayout;
     ViewPager mViewPager;
+    //final PagerAdapter pagerAdapter;
     TextView texto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setHomeAsUpIndicator(R.drawable.ic_launcher_background);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         tabLayout = findViewById(R.id.tabLayout);
@@ -41,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.addTab(tabLayout.newTab().setText("TELÉFONOS"));
         tabLayout.addTab(tabLayout.newTab().setText("TABLETS"));
         tabLayout.addTab(tabLayout.newTab().setText("PORTÁTILES"));
+
+        mViewPager = findViewById(R.id.viewpager);
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
 
     }
